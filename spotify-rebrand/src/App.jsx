4c51f9269a7fab4a8a7c548baa9dfb7f4612832b
nibmangit <nibretumengaw@gmail.com>
2025-12-21 02:55:07 +0430

    Initial commit

---
+++ spotify-rebrand/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="bg-[#121212] text-gray-500">
+            <h1>Setting up the environments</h1>
+        </div>
+    )
+}
+
+export default App
